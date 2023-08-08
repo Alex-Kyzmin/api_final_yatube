9ce26321f49f97b247ffd122182d87bcc62a2ac4
Александр Кузьмин <AlexanderKyzmin@yandex.ru>
2023-08-08 13:40:03 +0300

    Вариант до ревью

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,78 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
-
-    class Meta:
-        fields = '__all__'
-        model = Post
-
-
-class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор к модели постов"""
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        slug_field='username',
+        # автоназначение автора, чтобы не переопределять
+        # во вьюсет метод perform_create
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
     )
 
     class Meta:
+        model = Post
         fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор к модели комментарий"""
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        # автоназначение автора, чтобы не переопределять
+        # во вьюсет метод perform_create
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
+    )
+    post = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+    )
+
+    class Meta:
         model = Comment
+        fields = '__all__'
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    """Сериализатор к модели групп"""
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Follow."""
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        # автоназначение автора, чтобы не переопределять
+        # во вьюсет метод perform_create
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+    )
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Вы уже подписаны на этого пользователя',
+            )
+        ]
+
+    def validate(self, data):
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError(
+                'Вы пытаетесь подписаться на самого себя!'
+            )
+        return data
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,64 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, mixins, permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from api.permissions import IsAuthorOrReadOnly
+from api.serializers import (CommentSerializer, FollowSerializer,
+                             GroupSerializer, PostSerializer)
+from posts.models import Group, Post
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """ViewSet постов:
+    Поддерживает все CRUD запросы (action) к постам,
+    в зависимости от пермишен
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+    pagination_class = LimitOffsetPagination
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """ViewSet комментарий:
+    Поддерживает все CRUD запросы (action) к постам с комментариями,
+    в зависимости от пермишен
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+
+    # переопределяем объект запроса.
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        return post.comments.all()
+
+    # переопределяем объект создания action.
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        serializer.save(post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """ViewSet групп постов(только для чтения):
+    1. Cписок групп (List());
+    2. О конкретной группе по id (retrieve()).
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class FollowViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    """ViewSet подписки (только для авторизованных пользователей):
+    1. Cписок на подписанных авторов (List());
+    2. Подписаться на автора (create()).
+    """
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    # переопределяем объект запроса.
+    def get_queryset(self):
+        return self.request.user.follower.all()
