d7c7ae44c7d40cddb0d79087c73aa99dc807e5ac
Александр Кузьмин <AlexanderKyzmin@yandex.ru>
2024-02-12 20:35:11 +0300

    finaly version

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    """Разрешение для автора (остальные чтение)"""
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated)
